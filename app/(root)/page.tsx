c7cdaad4b134e3833589321b3dd23c7c7464d69f
gitakash08 <akash08vani@gmail.com>
2024-04-14 22:40:55 +0530

    inital commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { UserButton } from '@clerk/nextjs'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <p>Home</p>
+    </div>
+  )
+}
+
+export default Home
---
+++ app/(root)/transformations/[id]/update/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UpdateTranformationPage = () => {
+  return (
+    <div>UpdateTranformationPage</div>
+  )
+}
+
+export default UpdateTranformationPage
